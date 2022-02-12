ab0040a2e1e4fe9d736bc6006e2ff262fc1689e3
RATDOTL <ratdotl2357@gmail.com>
2022-02-12 11:41:25 +0900

    develop branchの作成

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ const App = () => {
           endDate={new Date("2022-12-31")}
 
           values={[
-            { date: "2022-07-03", count: 1 },
-            { date: "2022-08-22", count: 2 },
-            { date: "2022-07-29", count: 4 },
-            { date: '2022-10-01', count: 1 },
+            { date: "2022-01-03", count: 1 },
+            { date: "2022-02-22", count: 2 },
+            { date: "2022-03-29", count: 4 },
+            { date: '2022-04-01', count: 1 },
             { date: '2022-10-03', count: 2 },
             { date: '2022-10-06', count: 3 },
             { date: '2022-10-10', count: 4 },
